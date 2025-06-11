eb83b2b5e119c06c581033dd43e242e746c85735
IbrahimArmanes <ibay411@hotmail.fr>
2025-06-11 16:34:42 +0200

    test 8

--- back/src/main/java/com/openclassrooms/bobapp/controller/JokeController.java
+++ back/src/main/java/com/openclassrooms/bobapp/controller/JokeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.openclassrooms.bobapp.service.JokeService;
 
-// Testing a change 7
+// Testing a change 8
 @RestController
 @RequestMapping("api/joke")
 public class JokeController {
